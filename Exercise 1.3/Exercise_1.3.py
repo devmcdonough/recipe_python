a3d16b3f12eca23ea6c1125b81b7ff7f24a82b4e
devmcdonough <devmcdonough@gmail.com>
2024-08-19 19:30:16 -0400

    Exercise 1.3 - Fixes

--- Exercise 1.3/Exercise_1.3.py
+++ Exercise 1.3/Exercise_1.3.py
@@ -6,7 +6,7 @@ def take_recipe():
     n = int(input("How many recipes would you like to enter?: "))
 
     for i in range(n):
-        name = input("Name of recipe {i + 1}: ")
+        name = input(f"Name of recipe {i + 1}: ")
         cooking_time = int(input("Cooking time: "))
         ingredients = input("Enter your ingredients and separate them with commas: ").split(', ')
         
@@ -19,28 +19,29 @@ def take_recipe():
         elif cooking_time >= 10 and len(ingredients) >= 4:
             difficulty = 'Hard'
 
-            recipe = {'name': name, 'cooking_time': cooking_time, 'ingredients': ingredients, 'difficulty': difficulty}
+        recipe = {'name': name, 'cooking_time': cooking_time, 'ingredients': ingredients, 'difficulty': difficulty}
 
-            for ingredient in recipe['ingredients']:
-                if ingredient not in ingredients_list:
-                    ingredients_list.append(ingredient)
+        for ingredient in recipe['ingredients']:
+            if ingredient not in ingredients_list:
+                ingredients_list.append(ingredient)
         
 
-            recipes_list.append(recipe)
+        recipes_list.append(recipe)
 
-            print(f"Recipe {i + 1} added: {recipe}")
+        print(f"Recipe {i + 1} added: {recipe}")
 
 def display_recipes():
-    print("/nAll recipes:/n")
+    print("\nAll recipes:\n")
     for recipe in recipes_list:
         print(f"Recipe: {recipe['name']}")
-        print(f"Cooking Time: {recipe['cooking time']}")
+        print(f"Cooking Time: {recipe['cooking_time']}")
         print(f"Ingredients: {', '.join(recipe['ingredients'])}")
         print(f"Difficulty: {recipe['difficulty']}")
+        print("-" * 20)
 
 def display_ingredients():
     sorted_ingredients = sorted(ingredients_list)
-    print("/nIngredients Available Across All Recipes")
+    print("\nIngredients Available Across All Recipes")
     print("-" * 40)
     for ingredient in sorted_ingredients:
         print(ingredient)
